ca8442e4c428000c5386c9484b22df6582b7d162
Josh <joshuadronyi@gmail.com>
2019-09-18 03:33:43 -0400

    Added lifecycle methods and the log tags that accompany them.

--- app/src/main/java/com/example/tetrisscreenshot/MainActivity.java
+++ app/src/main/java/com/example/tetrisscreenshot/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tetrisscreenshot;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("creating","onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("starting","onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("resuming","onResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("pausing","onPause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("stopping","onStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("DESTROYING!!!!!","KILL EVERYONE AND EVERYTHING!!!!!!");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Restarting","Good thing we didn't kill everyone and everything, right?");
+
     }
 }
